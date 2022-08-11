7ef251c32cb0ffc4f40ad17c3b760d289cf62924
Sagarika Chakraborty <63808789+Sagarikac02@users.noreply.github.com>
2022-08-11 19:28:31 +0530

    Create matrixmultiplication.py

---
+++ matrixmultiplication.py
@@ -0,0 +1,26 @@
+r1 = [1,2,3]
+r2 = [4,5,6]
+r3 = [7,8,9]
+
+s1 = [1,2,1]
+s2 = [6,2,3]
+s3 = [4,2,1]
+
+A = []
+B = []
+A.append(r1)
+A.append(r2)
+A.append(r3)
+
+B.append(s1)
+B.append(s2)
+B.append(s3)
+
+C = [[0,0,0],[0,0,0],[0,0,0]]
+dim = 3
+for i in range(dim):
+  for j in range(dim):
+    for k in range(dim):
+      C[i][j] = C[i][j] + A[i][k]*B[k][j]
+print(C)
+
